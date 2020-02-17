25000629861c29cefa12830b08ad7f3a1587dc2d
Preidun <Preidun_1997@live.nl>
2020-02-17 12:35:53 +0100

    The FNNnewMW code is the new matlab code that has the bagging method implemented and has the Moving window design.

--- FNNnewMW.m
+++ FNNnewMW.m
@@ -1,4 +1,4 @@
-function [PCC, AUC, PG,BS, KS, H] = FNNnewMW(X1,X2,y1,y2)
+function [PCC_KS_AUC_PG_H_BS_FNNnewMW, PCC_KS_AUC_PG_H_BS_BaggingFNNnewMW] = FNNnewMW(X1,X2,y1,y2)
 %% This code uses feedforward neural networks (estimated with backpropagation and mini-batch stochastic gradient descent) to make classifications for the test sample
 % we do not make use of 4 fold cross validation (fold for each quarter of each
 % year). Also, we will use 5 fold cross validation to fine tune the
@@ -263,31 +263,327 @@ end
   KSvalue_vector(1) = KS_value;
   end
    
- %  for i = 1:size( X2,1 )
- %   [yhat,~,~,~] = Feedforward(Wvector1PCC,Wvector2PCC,bvec1PCC ,bvec2PCC,X2(i,:)' );
- %   predicted_probsPCC(i) = yhat;
- %    [yhat,~,~,~] = Feedforward(WvectorKS,Wvector2KS,bvec1KS ,bvec2KS,X2(i,:)' );
- %   predicted_probsKS(i) = yhat;
- %    [yhat,~,~,~] = Feedforward(Wvector1AUC,Wvector2AUC,bvec1AUC ,bvec2AUC,X2(i,:)' );
- %   predicted_probsAUC(i) = yhat;
- %    [yhat,~,~,~] = Feedforward(Wvector1PG,Wvector2PG,bvec1PG ,bvec2PG,X2(i,:)' );
- %   predicted_probsPG(i) = yhat;
- %    [yhat,~,~,~] = Feedforward(Wvector1H,Wvector2H,bvec1H ,bvec2H,X2(i,:)' );
- %   predicted_probsH(i) = yhat;
- %    [yhat,~,~,~] = Feedforward(Wvector1BS,Wvector2BS,bvec1BS ,bvec2BS,X2(i,:)' );
- %   predicted_probsBS(i) = yhat;
- %  end
-  
- 
-  
- 
-  
-  
-%% 
 PCC = mean(PCC_vector);
 AUC = mean(AUC_vector);
 PG  = mean(PGini_vector);
 BS  = mean(BScore_vector);
 H   = mean(Hmeasure_vector);
 KS  = mean(KSvalue_vector);
+
+
+PCC_KS_AUC_PG_H_BS_FNNnewMW = [PCC, KS, AUC, PG, H, BS];
+ 
+%% Do bagging on FNNs with previously obtained optimal hyperparameters.
+
+% Pre define patternet objects for each measure.
+netPCC = patternnet(lambda_and_hidNodes_optimal_PCC(2));
+netKS = patternnet(lambda_and_hidNodes_optimal_KS(2));
+netAUC = patternnet(lambda_and_hidNodes_optimal_AUC(2));
+netPG = patternnet(lambda_and_hidNodes_optimal_PG(2));
+netH = patternnet(lambda_and_hidNodes_optimal_H(2));
+netBS = patternnet(lambda_and_hidNodes_optimal_BS(2));
+
+% We note that the default loss function used in training the FNN is
+% the cross entropy and not the MSE.
+netPCC.divideFcn = 'divideblock'; % this function divides the training set into a training set and validation set.
+netPCC.divideParam.trainRatio = 85/100;
+netPCC.divideParam.valRatio = 15/100;
+netPCC.divideParam.testRatio = 0/100;
+netPCC.performParam.regularization = lambda_and_hidNodes_optimal_PCC(1); % set regularization parameter to prevent overfitting.
+netPCC.biasConnect(2)= 0;
+netPCC.layers{1}.transferFcn = 'tansig'; %default activation function in hidden layer is tansig function.
+netPCC.layers{2}.transferFcn = 'softmax';
+netPCC.trainFcn = 'trainscg'; %default function is 'trainscg', that is stochastic conjugate gradient, we can change it to 'trainbfg'.
+netPCC.performFcn = 'crossentropy';% default criterion is 'crossentropy', we can change it to 'mse' to chech which is more suitable.
+netPCC.trainParam.showWindow = 0;
+
+netKS.divideFcn = 'divideblock'; 
+netKS.divideParam.trainRatio = 85/100;
+netKS.divideParam.valRatio = 15/100;
+netKS.divideParam.testRatio = 0/100;
+netKS.performParam.regularization = lambda_and_hidNodes_optimal_KS(1); 
+netKS.biasConnect(2)= 0; 
+netKS.layers{1}.transferFcn = 'tansig'; %default activation function in hidden layer is tansig function.
+netKS.layers{2}.transferFcn = 'softmax';
+netKS.trainFcn = 'trainscg'; %default function is 'trainscg', that is stochastic conjugate gradient, we can change it to 'trainbfg'.
+netKS.performFcn = 'crossentropy';% default criterion is 'crossentropy', we can change it to 'mse' to chech which is more suitable.
+netKS.trainParam.showWindow = 0;
+
+netAUC.divideFcn = 'divideblock'; 
+netAUC.divideParam.trainRatio = 85/100;
+netAUC.divideParam.valRatio = 15/100;
+netAUC.divideParam.testRatio = 0/100;
+netAUC.trainParam.showWindow = 0;
+netAUC.performParam.regularization = lambda_and_hidNodes_optimal_AUC(1);
+netAUC.biasConnect(2)= 0;
+netAUC.layers{1}.transferFcn = 'tansig'; %default activation function in hidden layer is tansig function.
+netAUC.layers{2}.transferFcn = 'softmax';
+netAUC.trainFcn = 'trainscg'; %default function is 'trainscg', that is stochastic conjugate gradient, we can change it to 'trainbfg'.
+netAUC.performFcn = 'crossentropy';% default criterion is 'crossentropy', we can change it to 'mse' to chech which is more suitable.
+netAUC.trainParam.showWindow = 0;
+
+netPG.divideFcn = 'divideblock'; 
+netPG.divideParam.trainRatio = 85/100;
+netPG.divideParam.valRatio = 15/100;
+netPG.divideParam.testRatio = 0/100;
+netPG.performParam.regularization = lambda_and_hidNodes_optimal_PG(1);
+netPG.biasConnect(2)= 0;
+netPG.layers{1}.transferFcn = 'tansig'; %default activation function in hidden layer is tansig function.
+netPG.layers{2}.transferFcn = 'softmax';
+netPG.trainFcn = 'trainscg'; %default function is 'trainscg', that is stochastic conjugate gradient, we can change it to 'trainbfg'.
+netPG.performFcn = 'crossentropy';% default criterion is 'crossentropy', we can change it to 'mse' to chech which is more suitable.
+netPG.trainParam.showWindow = 0;
+
+netH.divideFcn = 'divideblock';
+netH.divideParam.trainRatio = 85/100;
+netH.divideParam.valRatio = 15/100;
+netH.divideParam.testRatio = 0/100;
+netH.performParam.regularization = lambda_and_hidNodes_optimal_H(1); 
+netH.biasConnect(2)= 0;
+netH.layers{1}.transferFcn = 'tansig'; %default activation function in hidden layer is tansig function.
+netH.layers{2}.transferFcn = 'softmax';
+netH.trainFcn = 'trainscg'; %default function is 'trainscg', that is stochastic conjugate gradient, we can change it to 'trainbfg'.
+netH.performFcn = 'crossentropy';% default criterion is 'crossentropy', we can change it to 'mse' to chech which is more suitable.
+netH.trainParam.showWindow = 0;
+
+netBS.divideFcn = 'divideblock'; 
+netBS.divideParam.trainRatio = 85/100;
+netBS.divideParam.valRatio = 15/100;
+netBS.divideParam.testRatio = 0/100;
+netBS.performParam.regularization = lambda_and_hidNodes_optimal_BS(1);
+netBS.biasConnect(2)= 0;
+netBS.layers{1}.transferFcn = 'tansig'; %default activation function in hidden layer is tansig function.
+netBS.layers{2}.transferFcn = 'softmax';
+netBS.trainFcn = 'trainscg'; %default function is 'trainscg', that is stochastic conjugate gradient, we can change it to 'trainbfg'.
+netBS.performFcn = 'crossentropy';% default criterion is 'crossentropy', we can change it to 'mse' to chech which is more suitable.
+netBS.trainParam.showWindow = 0;
+
+
+% Train the models where we split the training set Xtrain123 in to a train and
+% validation set which has the purpose to determine the appropriate
+% early stopping. Input [Xtrain123 XaDaSyn  is 100xN1 matrix and input y is 2XN1 (one row
+% for each class label, here first is defaulted loans, second is
+% non-defaulted loans).
+
+NumberBagsVector = [5 10 25 50];
+
+% columns are the six performance metrics and the rows are the four values
+% of the hyperparameter No. of bags.
+% PCC, KS ----- AUC, Hmeasure, PG index ----- BS (in this order).
+PerformanceMeasuresMatrix =  zeros(numel(NumberBagsVector),6);
+
+if useAdaSyn == 1
+    
+    %[XAdaSyn, yAda] = ADASYN(Xtrain123, ytrain123, 1, [], [], false);
+    
+    for numOfBags = 1:length(NumberBagsVector)
+        
+        PCCm = zeros(size(Xtest4,1),numOfBags);
+        KSm  = zeros(size(Xtest4,1),numOfBags);
+        AUCm = zeros(size(Xtest4,1),numOfBags);
+        PGm  = zeros(size(Xtest4,1),numOfBags);
+        Hm   = zeros(size(Xtest4,1),numOfBags);
+        BSm  = zeros(size(Xtest4,1),numOfBags);
+
+        for b=1:NumberBagsVector(numOfBags)
+            TrainMatrix=[[XAdaSyn;Xtrain123],[double(yAda);ytrain123]];
+            Lengthdata=size(TrainMatrix,1);
+            temp1=ceil(Lengthdata*rand(Lengthdata,1));
+            bsdata=TrainMatrix(temp1,:);
+            bsx=bsdata(:,1:end-1);
+            bsy=bsdata(:,end);
+
+
+            [netPCC,~] = train(netPCC,bsx', bsy');
+            [netKS,~] = train(netKS,bsx', bsy');
+            [netAUC,~] = train(netAUC,bsx', bsy');
+            [netPG,~] = train(netPG,bsx', bsy');
+            [netH,~] = train(netH, bsx', bsy');
+            [netBS,~] = train(netBS, bsx', bsy');
+
+            % Construct new probability score (using default softmax activation function in the output node) on test data.
+            predicted_probsPCC = netPCC(Xtest4');
+            predicted_probsPCC = predicted_probsPCC(1,:)';
+            PCCm(:,b) = predicted_probsPCC;
+
+            predicted_probsKS = netKS(Xtest4');
+            predicted_probsKS = predicted_probsKS(1,:)';
+            KSm(:,b) = predicted_probsKS;
+
+            predicted_probsH = netH(Xtest4');
+            predicted_probsH = predicted_probsH(1,:)';
+            Hm(:,b) = predicted_probsH;
+
+            predicted_probsPG = netPG(Xtest4');
+            predicted_probsPG = predicted_probsPG(1,:)';
+            PGm(:,b) = predicted_probsPG;
+
+            predicted_probsAUC = netAUC(Xtest4');
+            predicted_probsAUC = predicted_probsAUC(1,:)';
+            AUCm(:,b) = predicted_probsAUC;
+
+            predicted_probsBS = netBS(Xtest4');
+            predicted_probsBS = predicted_probsBS(1,:)';
+            BSm(:,b) = predicted_probsBS;
+
+        end
+
+        tempPCC = mean(PCCm,2);
+        tempKS=mean(KSm,2);
+        tempH=mean(Hm,2);
+        tempPG=mean(PGm,2);
+        tempAUC=mean(AUCm,2);
+        tempBS=mean(BSm,2);
+
+        
+        t = mean([double(yAda);ytrain123]);
+        YhatPCC = tempPCC > t;
+        % mean(predicted_probsPCC)
+        % function that computes the PCC, requires real y-values, predicted_y
+        % values.
+        PCC =  sum( (Ytest4 == YhatPCC) )/numel(Ytest4);
+        PerformanceMeasuresMatrix(numOfBags,1) = PCC;
+
+        
+        prior1 = mean([double(yAda);ytrain123]); prior0 = 1 - prior1;
+
+        [AUC,~, ~ ] = computeAUC_PGindex_Hvalue(Ytest4, tempAUC, prior1, prior0);
+        PerformanceMeasuresMatrix(numOfBags,3) = AUC;
+        
+
+        [~,PG_index, ~ ] = computeAUC_PGindex_Hvalue(Ytest4, tempPG, prior1, prior0);
+        PerformanceMeasuresMatrix(numOfBags,5) = PG_index;
+        
+
+        [~,~, H_measure ] = computeAUC_PGindex_Hvalue(Ytest4, tempH, prior1, prior0);
+        PerformanceMeasuresMatrix(numOfBags,4) = H_measure ;
+
+
+        BScore = mean( (tempBS - Ytest4).^2);
+        PerformanceMeasuresMatrix(numOfBags,6) = BScore;
+
+        KS_value = computeKSvalue(Ytest4,tempKS);
+        PerformanceMeasuresMatrix(numOfBags,2) = KS_value ;
+    
+    end
+    
+    PCC_vector(1) = max( PerformanceMeasuresMatrix(:,1));
+    KSvalue_vector(1) = max( PerformanceMeasuresMatrix(:,2));
+    AUC_vector(1) = max( PerformanceMeasuresMatrix(:,3));
+    Hmeasure_vector(1) = max( PerformanceMeasuresMatrix(:,4));
+    PGini_vector(1) = max( PerformanceMeasuresMatrix(:,5));
+    BScore_vector(1) = min( PerformanceMeasuresMatrix(:,6));
+    
+else
+    for numOfBags = 1:length(NumberBagsVector)
+        
+        PCCm = zeros(size(Xtest4,1),numOfBags);
+        KSm  = zeros(size(Xtest4,1),numOfBags);
+        AUCm = zeros(size(Xtest4,1),numOfBags);
+        PGm  = zeros(size(Xtest4,1),numOfBags);
+        Hm   = zeros(size(Xtest4,1),numOfBags);
+        BSm  = zeros(size(Xtest4,1),numOfBags);
+
+        for b=1:NumberBagsVector(numOfBags)
+            TrainMatrix=[Xtrain123,ytrain123];
+            Lengthdata=size(TrainMatrix,1);
+            temp1=ceil(Lengthdata*rand(Lengthdata,1));
+            bsdata=TrainMatrix(temp1,:);
+            bsx=bsdata(:,1:end-1);
+            bsy=bsdata(:,end);
+
+
+            [netPCC,~] = train(netPCC,bsx', bsy');
+            [netKS,~] = train(netKS,bsx', bsy');
+            [netAUC,~] = train(netAUC,bsx', bsy');
+            [netPG,~] = train(netPG,bsx', bsy');
+            [netH,~] = train(netH, bsx', bsy');
+            [netBS,~] = train(netBS, bsx', bsy');
+
+            % Construct new probability score (using default softmax activation function in the output node) on test data.
+            predicted_probsPCC = netPCC(Xtest4');
+            predicted_probsPCC = predicted_probsPCC(1,:)';
+            PCCm(:,b) = predicted_probsPCC;
+
+            predicted_probsKS = netKS(Xtest4');
+            predicted_probsKS = predicted_probsKS(1,:)';
+            KSm(:,b) = predicted_probsKS;
+
+            predicted_probsH = netH(Xtest4');
+            predicted_probsH = predicted_probsH(1,:)';
+            Hm(:,b) = predicted_probsH;
+
+            predicted_probsPG = netPG(Xtest4');
+            predicted_probsPG = predicted_probsPG(1,:)';
+            PGm(:,b) = predicted_probsPG;
+
+            predicted_probsAUC = netAUC(Xtest4');
+            predicted_probsAUC = predicted_probsAUC(1,:)';
+            AUCm(:,b) = predicted_probsAUC;
+
+            predicted_probsBS = netBS(Xtest4');
+            predicted_probsBS = predicted_probsBS(1,:)';
+            BSm(:,b) = predicted_probsBS;
+
+        end
+
+        tempPCC=mean(PCCm,2);
+        tempKS=mean(KSm,2);
+        tempH=mean(Hm,2);
+        tempPG=mean(PGm,2);
+        tempAUC=mean(AUCm,2);
+        tempBS=mean(BSm,2);
+
+        
+        t = mean(ytrain123);
+        YhatPCC = tempPCC > t;
+        % mean(predicted_probsPCC)
+        % function that computes the PCC, requires real y-values, predicted_y
+        % values.
+        PCC =  sum( (Ytest4 == YhatPCC) )/numel(Ytest4);
+        PCC_vector(1) = PCC;
+        PerformanceMeasuresMatrix(numOfBags,1) = PCC;
+
+        
+        prior1 = mean(ytrain123); prior0 = 1 - prior1;
+
+        [AUC,~, ~ ] = computeAUC_PGindex_Hvalue(Ytest4, tempAUC, prior1, prior0);
+        PerformanceMeasuresMatrix(numOfBags,3) = AUC;
+        
+
+        [~,PG_index, ~ ] = computeAUC_PGindex_Hvalue(Ytest4, tempPG, prior1, prior0);
+        PerformanceMeasuresMatrix(numOfBags,5) = PG_index;
+        
+
+        [~,~, H_measure ] = computeAUC_PGindex_Hvalue(Ytest4, tempH, prior1, prior0);
+        PerformanceMeasuresMatrix(numOfBags,4) = H_measure ;
+
+
+        BScore = mean( (tempBS - Ytest4).^2);
+        PerformanceMeasuresMatrix(numOfBags,6) = BScore;
+
+        KS_value = computeKSvalue(Ytest4,tempKS);
+        PerformanceMeasuresMatrix(numOfBags,2) = KS_value ;
+    
+    end
+    
+    PCC_vector(1) = max( PerformanceMeasuresMatrix(:,1));
+    KSvalue_vector(1) = max( PerformanceMeasuresMatrix(:,2));
+    AUC_vector(1) = max( PerformanceMeasuresMatrix(:,3));
+    Hmeasure_vector(1) = max( PerformanceMeasuresMatrix(:,4));
+    PGini_vector(1) = max( PerformanceMeasuresMatrix(:,5));
+    BScore_vector(1) = min( PerformanceMeasuresMatrix(:,6));
+    
+end
+
+
+PCC = mean(PCC_vector);
+AUC = mean(AUC_vector);
+PG  = mean(PGini_vector);
+BS  = mean(BScore_vector);
+H   = mean(Hmeasure_vector);
+KS  = mean(KSvalue_vector);
+
+PCC_KS_AUC_PG_H_BS_BaggingFNNnewMW = [PCC, KS, AUC,PG,H, BS];
+
 end
